b0068ebf2676eff4f91f14467fc8a27d3a34261b
Daeun Kim <74234333+dar-jeeling@users.noreply.github.com>
2022-07-09 22:57:30 +0900

    [Silver III] Title: N과 M (1), Time: 2448 ms, Memory: 13672 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -0,0 +1,34 @@
+const inputData = () => {
+  const input = require("fs");
+  const data = input
+    .readFileSync("/dev/stdin")
+    .toString()
+    .split(" ")
+    .map((number) => parseInt(number));
+  return data;
+};
+
+const [N, M] = inputData();
+const visited = new Array(N + 1).fill(false);
+
+const findSequence = (answer, visited) => {
+  if (answer.length === M) {
+    console.log(answer.join(" "));
+    return;
+  } else {
+    for (let i = 1; i <= N; i++) {
+      if (visited[i]) continue;
+      answer.push(i);
+      visited[i] = true;
+      findSequence(answer, visited);
+      answer.pop();
+      visited[i] = false;
+    }
+  }
+};
+
+const main = () => {
+  findSequence([], visited);
+};
+
+main();
