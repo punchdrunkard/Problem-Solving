ef8ddd8c8467ca66dc997527b3e3f153f9180186
Daeun Kim <74234333+dar-jeeling@users.noreply.github.com>
2022-09-29 23:58:57 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N;
+  cin >> N;
+
+  for (int i = 1; i < N + 1; i++) {
+    for (int j = 0; j < N - i; j++) {
+      cout << ' ';
+    }
+
+    for (int j = 0; j < 2 * i - 1; j++) {
+      cout << '*';
+    }
+
+    cout << '\n';
+  }
+  return 0;
+}
