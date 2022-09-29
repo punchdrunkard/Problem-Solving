7883b95654df5c5be86d574852730be63def7d16
Daeun Kim <74234333+dar-jeeling@users.noreply.github.com>
2022-09-29 21:45:07 +0900

    [Bronze III] Title: 윷놀이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.cc
+++ 백준/Bronze/2490. 윷놀이/윷놀이.cc
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -9,41 +6,17 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int result, input;
+    string res = "DCBAE";
+
     for (int i = 0; i < 3; i++)
     {
-        // 0 배 1 등
-        // 도(A) : 배 1, 등 3 -> 3
-        // 개 B: 배 2 등 2 -> 2
-        // 걸 C1
-        // 윷 D 0
-        // 모 E 4
-        int a, b, c, d;
-        cin >> a >> b >> c >> d;
-
-        int sum = a + b + c + d;
-
-        switch (sum)
+        result = 0;
+        for (int j = 0; j < 4; j++)
         {
-        case 1:
-            cout << "C"
-                 << "\n";
-            break;
-        case 2:
-            cout << "B"
-                 << "\n";
-            break;
-        case 3:
-            cout << "A"
-                 << "\n";
-            break;
-        case 4:
-            cout << "E"
-                 << "\n";
-            break;
-        case 0:
-            cout << "D"
-                 << "\n";
-            break;
+            cin >> input;
+            result += input;
         }
+        cout << res[result] << '\n';
     }
 }
