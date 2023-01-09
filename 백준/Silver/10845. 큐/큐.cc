2ebbf3d190a5027870936adff45f64ad6b490c60
Daeun Kim <74234333+dar-jeeling@users.noreply.github.com>
2023-01-09 10:37:24 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -1,51 +1,81 @@
 #include <bits/stdc++.h>
-
-#include <queue>
+#define MAX 10001
 using namespace std;
+
+vector<string> op = {"push", "pop", "size", "empty", "front", "back"};
+
+int get_type(string w) {
+  for (int i = 0; i < op.size(); i++) {
+    if (w == op[i]) return (i);
+  }
+  return (-1);  // 없는 경우
+}
+
+int n;
+
+int q[MAX];
+int front_index;
+int back_index;
+
+void push(int x) {
+  q[back_index++] = x;
+}
+
+int pop() {
+  if (front_index == back_index) return -1;
+  return q[front_index++];
+}
+
+int size() { return back_index - front_index; }
+
+int empty() { return front_index == back_index; }
+
+int front() {
+  if (empty()) return -1;
+  return q[front_index];
+}
+
+int back() {
+  if (empty()) return -1;
+  return q[back_index - 1];
+}
 
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int N;
-  cin >> N;
+  cin >> n;
 
-  queue<int> q;
-
-  while (N--) {
+  while (n--) {
     string op;
+    int type;
+
     cin >> op;
-    if (op == "push") {
-      int add;
-      cin >> add;
-      q.push(add);
-    } else if (op == "pop") {
-      if (q.size() != 0) {
-        cout << q.front() << '\n';
-        q.pop();
-      } else {
-        cout << -1 << '\n';
-      }
-    } else if (op == "size") {
-      cout << q.size() << '\n';
-    } else if (op == "empty") {
-      if (q.size() == 0) {
-        cout << 1 << '\n';
-      } else {
-        cout << 0 << '\n';
-      }
-    } else if (op == "front") {
-      if (q.size() != 0) {
-        cout << q.front() << '\n';
-      } else {
-        cout << -1 << '\n';
-      }
-    } else if (op == "back") {
-      if (q.size() != 0) {
-        cout << q.back() << '\n';
-      } else {
-        cout << -1 << '\n';
-      }
+    type = get_type(op);
+
+    switch (type) {
+      case 0:
+        int x;
+        cin >> x;
+        push(x);
+        break;
+      case 1:
+        cout << pop() << '\n';
+        break;
+      case 2:
+        cout << size() << '\n';
+        break;
+      case 3:
+        cout << empty() << '\n';
+        break;
+      case 4:
+        cout << front() << '\n';
+        break;
+      case 5:
+        cout << back() << '\n';
+        break;
     }
   }
+
+  return 0;
 }
