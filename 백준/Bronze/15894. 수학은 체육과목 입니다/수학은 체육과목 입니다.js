e4f0657e2fb69eb8996134c0c37b39eb09a82a07
Daeun Kim <74234333+dar-jeeling@users.noreply.github.com>
2023-01-13 03:06:40 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 144 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+console.log((BigInt(stdin[0]) * BigInt(4)).toString());
