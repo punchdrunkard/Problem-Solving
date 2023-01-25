ea2b901eac76f24d1446392cfa7c3ebfbe8b79ce
dar-jeeling <o_o___t@pusan.ac.kr>
2023-01-25 21:57:38 +0900

    comment: BOJ 2075 풀이 주석 추가

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 힙을 이용하여 N번 째 큰 수 찾기
+// 크기가 n인 최소 힙을 만들고 원소들을 차례대로 넣고
+// 크기가 n이 넘는 순간 위에서 부터 원소들을 뺀다면
+// 그 전에 있던 작은 원소들은 모두 빠져나가고
+// 마지막에는 n번째 큰 수가 제일 위에 있는 구조가 된다.
+
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
