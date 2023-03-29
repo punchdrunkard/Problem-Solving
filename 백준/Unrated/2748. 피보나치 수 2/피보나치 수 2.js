af8c00c6ef4e1fff78ebfabd10690642e3439ab8
Daeun Kim <74234333+dar-jeeling@users.noreply.github.com>
2023-03-29 15:56:29 +0900

    [Unrated] Title: 피보나치 수 2, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Unrated/2748. 피보나치 수 2/피보나치 수 2.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const stdin = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const n = parseInt(stdin[0]);
+
+// fibo[n] = n번째 피보나치 수
+const fibo = Array.from({ length: n + 1 }, () => {
+  return 0n;
+});
+
+// base case
+fibo[1] = 1n;
+
+// bottom-up
+for (let i = 2; i <= n; i++) {
+  fibo[i] = fibo[i - 2] + fibo[i - 1];
+}
+
+console.log(fibo[n].toString());
