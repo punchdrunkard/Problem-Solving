e91d7cf9fd33620a9a122a341f43c21fad4a7011
dar-jeeling <o_o___t@pusan.ac.kr>
2023-05-07 01:57:42 +0900

    comment: 2620 주석 추가

--- LeetCode/JS_Challenge/Day2/2620 Counter.js
+++ LeetCode/JS_Challenge/Day2/2620 Counter.js
@@ -16,3 +16,14 @@ let createCounter = function (n) {
  * counter() // 11
  * counter() // 12
  */
+
+// Don't Just Solve, But Learn: Solutions and Concepts for 30 Days of LC JavaScript Challenge
+// Day 2:
+// what is the benefit of returning nested function instead of just an incremented value?
+// Answer:
+// This is a good example of closure.
+
+// One benefit of returning a nested function instead of just an incremented value is that it allows you to create multiple independent counters, each with its own internal state, without having to create a separate function for each one.
+
+// A closure is a function that has access to variables in its outer (enclosing) function's scope chain.
+// In this code, the inner function that is returned by the "createCounter" function is a closure, and it has access to the "n" variable in the outer "createCounter" function's scope.
