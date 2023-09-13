9047862bf46625e8ad32c61405e7966fe5d2f0eb
Daeun Kim <74234333+dar-jeeling@users.noreply.github.com>
2023-09-13 14:36:05 +0900

    [Gold II] Title: 2048 (Easy), Time: 20 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
@@ -50,96 +50,44 @@ void makeSequence(vector<int> sequence) {
   }
 }
 
-enum Direction { LEFT = 0, RIGHT = 1, DOWN = 2, UP = 3 };
+enum Direction { LEFT = 0, DOWN = 1, RIGHT = 2, UP = 3 };
 
-// 배열을 돌린다.
-void rotate(vector<vector<int>> &board) {
+void rotate90Degree(vector<vector<int>> &board) {
   vector<vector<int>> temp = board;
 
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
-      board[j][i] = temp[i][j];
+      board[n - j - 1][i] = temp[i][j];
     }
   }
 }
 
 void move(vector<vector<int>> &board, int dir) {
-  // 한 번의 iteration : 해당 블록을 합치고, 해당 블록 이전의 부분을 민다.
-  vector<bool> merged(n, false);
-  vector<int> temp(n, 0);  // 이동한 결과를 담을 임시 배열
-
-  if (dir == UP || dir == DOWN) {
-    rotate(board);
+  for (int i = 0; i < dir; i++) {
+    rotate90Degree(board);
   }
 
-  if (dir == LEFT || dir == UP) {
-    for (int i = 0; i < n; i++) {  // 각 행에 대하여
-      for (int j = 0; j < n; j++) {
-        if (board[i][j] == 0) {  // 현재 빈칸이면 넘어간다.
-          continue;
-        }
+  for (int i = 0; i < n; i++) {  // 각 행에 대하여
+    int idx = 0;                 // 마지막으로 처리된 인덱스
+    vector<int> temp(n, 0);
 
-        if (temp[0] == 0) {  // 처음으로 원소가 밀리는 경우
-          temp[0] = board[i][j];
-          continue;
-        }
-
-        // 빈칸이 아니라면, 왼쪽으로 갈 수 있을만큼 넘긴다.
-        int idx = j;
-
-        while (temp[idx] == 0) {
-          idx--;
-        }
-
-        // 합칠 수 있는가?
-        if (!merged[idx] && temp[idx] == board[i][j]) {
-          temp[idx] *= 2;
-          merged[idx] = true;
-        } else {  // 합칠 수 없다면 빈칸에 저장한다.
-          temp[idx + 1] = board[i][j];
-        }
+    for (int j = 0; j < n; j++) {
+      if (board[i][j] == 0) {
+        continue;
       }
 
-      board[i] = temp;
-      fill(temp.begin(), temp.end(), 0);
-      fill(merged.begin(), merged.end(), false);
-    }
-  }
-
-  if (dir == RIGHT || dir == DOWN) {
-    for (int i = 0; i < n; i++) {
-      for (int j = n - 1; j >= 0; j--) {
-        if (board[i][j] == 0) {
-          continue;
-        }
-
-        if (temp[n - 1] == 0) {
-          temp[n - 1] = board[i][j];
-          continue;
-        }
-
-        int idx = j;
-        while (temp[idx] == 0) {
-          idx++;
-        }
-
-        if (!merged[idx] && temp[idx] == board[i][j]) {
-          temp[idx] *= 2;
-          merged[idx] = true;
-        } else {
-          temp[idx - 1] = board[i][j];
-        }
+      if (temp[idx] == 0) {  // 빈칸인 경우
+        temp[idx] = board[i][j];
+      } else if (temp[idx] == board[i][j]) {
+        temp[idx] *= 2;
+        idx++;
+      } else {
+        ++idx;
+        temp[idx] = board[i][j];
       }
-
-      board[i] = temp;
-
-      fill(temp.begin(), temp.end(), 0);
-      fill(merged.begin(), merged.end(), false);
     }
-  }
 
-  if (dir == UP || dir == DOWN) {
-    rotate(board);
+    board[i] = temp;
   }
 }
 
