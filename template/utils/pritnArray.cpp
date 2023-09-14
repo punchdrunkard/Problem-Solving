eab8ac23885e85590eeb375e0c857d1f09de7010
dar-jeeling <o_o___t@pusan.ac.kr>
2023-09-14 11:44:42 +0900

    util: printArray 함수 추가

---
+++ template/utils/pritnArray.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 타입 상관없이 2d array를 출력함
+template <typename T>
+void printArr(vector<vector<T>> &arr) {
+  for (int i = 0; i < arr.size(); i++) {
+    for (int j = 0; j < arr.size(); j++) {
+      cout << arr[i][j] << " ";
+    }
+    cout << "\n";
+  }
+
+  cout << "\n";
+};
+
+int main() { return 0; }
