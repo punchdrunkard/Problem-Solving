c8a68fc4f5e7a2acec1b5bd53c4521100e1eddb0
Daeun Kim <74234333+dar-jeeling@users.noreply.github.com>
2023-11-22 11:59:07 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 132 ms, Memory: 10360 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -5,53 +5,49 @@
 using namespace std;
 
 int n, m;
-vector<vector<int>> table, dp;
+vector<vector<int>> board, pSum;
 
 void input() {
   FASTIO;
+
+  // 테스트를 위한 파일 입력 코드 (TODO: 제출 전 삭제)
+  // freopen("sample_input.txt", "r", stdin);
+
   cin >> n >> m;
-  table.resize(n + 1, vector<int>(n + 1));
+  board.resize(n + 1, vector<int>(n + 1, 0));  // 1-indexed
+  // pSum[i][j] = (0, 0) ~ (i, j) 까지의 구간 합
+  pSum.resize(n + 1, vector<int>(n + 1, 0));
 
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= n; j++) {
-      cin >> table[i][j];
+      cin >> board[i][j];
     }
   }
 }
 
-void makeDPTable() {
-  dp.resize(n + 1, vector<int>(n + 1));
-
+void makePsum() {
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= n; j++) {
-      if (j == 1) {
-        dp[i][j] = table[i][j];
-      } else {
-        dp[i][j] = dp[i][j - 1] + table[i][j];
-      }
+      pSum[i][j] =
+          pSum[i - 1][j] + pSum[i][j - 1] + board[i][j] - pSum[i - 1][j - 1];
     }
   }
 }
 
-int query(int x1, int y1, int x2, int y2) {
-  int sum = 0;
-
-  for (int x = x1; x <= x2; x++) {
-    sum += (dp[x][y2] - dp[x][y1 - 1]);
-  }
-
-  return sum;
+int solve(int x1, int y1, int x2, int y2) {
+  return pSum[x2][y2] - pSum[x1 - 1][y2] - pSum[x2][y1 - 1] +
+         pSum[x1 - 1][y1 - 1];
 }
 
 int main() {
   input();
-  makeDPTable();
+  makePsum();
 
   for (int i = 0; i < m; i++) {
     int x1, y1, x2, y2;
     cin >> x1 >> y1 >> x2 >> y2;
 
-    cout << query(x1, y1, x2, y2) << '\n';
+    cout << solve(x1, y1, x2, y2) << '\n';
   }
 
   return 0;
