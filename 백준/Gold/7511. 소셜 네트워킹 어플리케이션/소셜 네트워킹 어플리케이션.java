5c5bc47222a6afaa0762db95849bfffefd8f85e9
42 <74234333+dar-jeeling@users.noreply.github.com>
2024-01-04 13:31:55 +0900

    [Gold V] Title: 소셜 네트워킹 어플리케이션, Time: 724 ms, Memory: 154532 KB -BaekjoonHub

--- 백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.java
+++ 백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.java
@@ -2,49 +2,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
 public class Main {
+    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
     static int[] parent;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         int testCase = Integer.parseInt(bufferedReader.readLine());
 
         for (int tc = 1; tc <= testCase; tc++) {
             sb.append("Scenario ").append(tc).append(":").append('\n');
-
-            int n = Integer.parseInt(bufferedReader.readLine());
-            parent = new int[n];
-
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
-            }
-
-            int k = Integer.parseInt(bufferedReader.readLine());
-
-            for (int i = 0; i < k; i++) {
-                StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                union(a, b);
-            }
-
-            int m = Integer.parseInt(bufferedReader.readLine());
-
-            for (int i = 0; i < m; i++){
-                StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-
-                sb.append(find(a) == find(b) ? 1 : 0).append('\n');
-            }
-
+            solve();
             sb.append('\n');
         }
 
         System.out.println(sb);
+    }
+
+    public static void solve() throws IOException {
+        int n = Integer.parseInt(bufferedReader.readLine());
+        parent = IntStream.range(0, n).toArray();
+
+        int k = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < k; i++) {
+            int[] edge = readPair(bufferedReader);
+            union(edge[0], edge[1]);
+        }
+
+        int m = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < m; i++){
+            int[] query = readPair(bufferedReader);
+            sb.append(find(query[0]) == find(query[1]) ? 1 : 0).append('\n');
+        }
     }
 
     public static void union(int a, int b) {
@@ -59,5 +50,10 @@ public class Main {
             parent[a] = find(parent[a]);
         }
         return parent[a];
+    }
+
+    private static int[] readPair(BufferedReader bufferedReader) throws IOException {
+        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+        return new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
     }
 }
