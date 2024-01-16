92defa5acb002face55207c2527ff3e3f81a855f
42 <74234333+dar-jeeling@users.noreply.github.com>
2024-01-16 14:23:48 +0900

    [Gold IV] Title: 주난의 난(難), Time: 224 ms, Memory: 20532 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.StringTokenizer;
 import java.util.Queue;
 
@@ -12,86 +14,68 @@ public class Main {
 	// 벽일 경우 = 거리가 0, 인간일 경우 = 거리가 1
 	static char[][] classroom;
 
-	static class State implements Comparable<State> {
+	static class Point {
 		int x;
 		int y;
-		int jumpCount; // 현재 점프 횟수
 
-		public State(int x, int y, int jumpCount) {
+		public Point(int x, int y){
 			this.x = x;
 			this.y = y;
-			this.jumpCount = jumpCount;
-		}
-
-		@Override
-		public int compareTo(State o) { // 최소 힙
-			return this.jumpCount - o.jumpCount;
-		}
-
-		@Override
-		public String toString() {
-			return String.format("(x : %d, y : %d, jumpCount: %d}", x, y, jumpCount);
 		}
 	}
 
 	public static void main(String[] args) throws IOException {
 		input();
-		System.out.println(dijkstra());
+		System.out.println(bfs());
 	}
 
 	// (x1, y1) ~ (x2, y2) 까지 가는 최단거리
-	public static int dijkstra() {
+	public static int bfs() {
 		int[] DX = {-1, 1, 0, 0};
 		int[] DY = {0, 0, -1, 1};
 
-		Queue<State> pq = new PriorityQueue<>();
 		int[][] dist = new int[n][m];
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				dist[i][j] = Integer.MAX_VALUE;
-			}
+		for (int i = 0; i < n; i++){
+			// 아직 방문하지 않았다면 -1
+			Arrays.fill(dist[i], -1);
 		}
 
-
-		pq.offer(new State(x1, y1, 0));
+		Deque<Point> q = new ArrayDeque<>();
+		q.offer(new Point(x1, y1));
 		dist[x1][y1] = 0;
 
-		while (!pq.isEmpty()) {
-			var current = pq.poll();
+		while (!q.isEmpty()){
+			Point current = q.poll();
 
-			if (dist[current.x][current.y] < current.jumpCount) {
-				continue;
-			}
-
-			// 답을 찾은 경우
+			// 도착한 경우
 			if (current.x == x2 && current.y == y2) {
-				return current.jumpCount;
+				return dist[x2][y2];
 			}
 
-			for (int dir = 0; dir < 4; dir++) {
+			for (int dir = 0; dir < 4; dir++){
 				int nx = current.x + DX[dir];
 				int ny = current.y + DY[dir];
 
-				if (!isValidRange(nx, ny))
-					continue;
+				if (!isValidRange(nx, ny)) continue;
+				if (dist[nx][ny] != -1) continue; // 이미 방문한 경우
 
-				int nextDist = current.jumpCount;
+				int nextDist = dist[current.x][current.y];
 
-				if (classroom[nx][ny] != '0') {
-					nextDist += 1;
+				if (classroom[nx][ny] != '0'){ // 가중치가 1
+					nextDist++;
+					q.offerLast(new Point(nx, ny));
+				} else {
+					// 가중치가 0인 경우, 먼저 탐색될 수 있도록 한다.
+					q.offerFirst(new Point(nx, ny));
 				}
 
-				if (dist[nx][ny] <= nextDist)
-					continue;
-
-				pq.offer(new State(nx, ny, nextDist));
 				dist[nx][ny] = nextDist;
 			}
 		}
-
 		return dist[x2][y2];
 	}
+
 
 	public static boolean isValidRange(int x, int y) {
 		return 0 <= x && x < n && 0 <= y && y < m;
@@ -127,10 +111,6 @@ public class Main {
 
 		public int nextInt() {
 			return Integer.parseInt(st.nextToken());
-		}
-
-		public long nextLong(StringTokenizer st) {
-			return Long.parseLong(st.nextToken());
 		}
 
 		public void setNewLine(String line) {
