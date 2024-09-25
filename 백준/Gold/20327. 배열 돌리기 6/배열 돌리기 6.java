9d40716ab2415c953ed57eaae1dc30e4280b2375
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2024-09-25 23:32:10 +0900

    [Gold II] Title: 배열 돌리기 6, Time: 588 ms, Memory: 21668 KB -BaekjoonHub

--- 백준/Gold/20327. 배열 돌리기 6/배열 돌리기 6.java
+++ 백준/Gold/20327. 배열 돌리기 6/배열 돌리기 6.java
@@ -4,15 +4,11 @@ import java.io.*;
 public class Main {
 
 	static FastReader scan = new FastReader();
-	// 주어진 배열
 
-	static int n, r;
-	static int[][] board, temp;
-
-	static int boardLength;
+	static int n, r, length;
+	static int[][] original, temp;
 
 	public static void main(String[] args) {
-
 		init();
 
 		for (int i = 0; i < r; i++) {
@@ -22,197 +18,178 @@ public class Main {
 			solve(k, l);
 		}
 
-		printBoard();
+		printAnswer();
 	}
 
-	static void solve(int opNum, int stage) {
-		copyOriginalBoardToTemp();
-		int subArrayLength = 1 << stage;
+	static void solve(int opNum, int l) {
+		int unitLength = 1 << l;
+		copyOriginalToTemp();
 
 		if (opNum == 1) {
-			op1(subArrayLength);
+			op1(unitLength);
 			return;
 		}
 
 		if (opNum == 2) {
-			op2(subArrayLength);
+			op2(unitLength);
 			return;
 		}
 
 		if (opNum == 3) {
-			op3(subArrayLength);
+			op3(unitLength);
 			return;
 		}
 
 		if (opNum == 4) {
-			op4(subArrayLength);
+			op4(unitLength);
 			return;
 		}
 
 		if (opNum == 5) {
-			op5(subArrayLength);
+			op5(unitLength);
 			return;
 		}
 
 		if (opNum == 6) {
-			op6(subArrayLength);
+			op6(unitLength);
 			return;
 		}
 
 		if (opNum == 7) {
-			op7(subArrayLength);
+			op7(unitLength);
 			return;
 		}
 
 		if (opNum == 8) {
-			op8(subArrayLength);
+			op8(unitLength);
 			return;
 		}
 	}
 
-	// 각 부분 배열을 상하 반전시킨다.
-	static void op1(int subArrayLength) {
-		for (int startX = 0; startX < boardLength; startX += subArrayLength) {
-			for (int startY = 0; startY < boardLength; startY += subArrayLength) {
-
-				for (int x = 0; x < subArrayLength; x++) {
-					for (int y = 0; y < subArrayLength; y++) {
-						board[x + startX][y + startY] = temp[startX + subArrayLength - x - 1][y + startY];
+	// 상하 반전
+	static void op1(int unitLength) {
+		for (int startX = 0; startX < length; startX += unitLength) {
+			for (int startY = 0; startY < length; startY += unitLength) {
+				for (int x = 0; x < unitLength; x++) {
+					for (int y = 0; y < unitLength; y++) {
+						original[startX + x][startY + y] = temp[startX + unitLength - x - 1][startY + y];
 					}
 				}
 			}
 		}
 	}
 
-	// 각 부분 배열을 좌우 반전시킨다.
-	static void op2(int subArrayLength) {
-		for (int startX = 0; startX < boardLength; startX += subArrayLength) {
-			for (int startY = 0; startY < boardLength; startY += subArrayLength) {
-
-				for (int x = 0; x < subArrayLength; x++) {
-					for (int y = 0; y < subArrayLength; y++) {
-						board[startX + x][startY + y] = temp[startX + x][startY + (subArrayLength - y - 1)];
+	// 좌우 반전
+	static void op2(int unitLength) {
+		for (int startX = 0; startX < length; startX += unitLength) {
+			for (int startY = 0; startY < length; startY += unitLength) {
+				for (int x = 0; x < unitLength; x++) {
+					for (int y = 0; y < unitLength; y++) {
+						original[startX + x][startY + y] = temp[startX + x][startY + unitLength - y - 1];
 					}
 				}
 			}
 		}
 	}
 
-	// 반시계 90도 회전
-	static void op3(int subArrayLength) {
-		for (int startX = 0; startX < boardLength; startX += subArrayLength) {
-			for (int startY = 0; startY < boardLength; startY += subArrayLength) {
-
-				for (int x = 0; x < subArrayLength; x++) {
-					for (int y = 0; y < subArrayLength; y++) {
-						board[startX + x][startY + y] = temp[startX + subArrayLength - y - 1][startY + x];
+	// 반시계방향 90도 회전
+	static void op3(int unitLength) {
+		for (int startX = 0; startX < length; startX += unitLength) {
+			for (int startY = 0; startY < length; startY += unitLength) {
+				for (int x = 0; x < unitLength; x++) {
+					for (int y = 0; y < unitLength; y++) {
+						original[startX + x][startY + y] = temp[startX + unitLength - y - 1][startY + x];
 					}
 				}
 			}
 		}
 	}
 
-	// 시계 90도 회전
-	static void op4(int subArrayLength) {
-		for (int startX = 0; startX < boardLength; startX += subArrayLength) {
-			for (int startY = 0; startY < boardLength; startY += subArrayLength) {
-
-				for (int x = 0; x < subArrayLength; x++) {
-					for (int y = 0; y < subArrayLength; y++) {
-						board[startX + x][startY + y] = temp[startX + y][startY + subArrayLength - x - 1];
+	// 시계방향 90도 회전
+	static void op4(int unitLength) {
+		for (int startX = 0; startX < length; startX += unitLength) {
+			for (int startY = 0; startY < length; startY += unitLength) {
+				for (int x = 0; x < unitLength; x++) {
+					for (int y = 0; y < unitLength; y++) {
+						original[startX + x][startY + y] = temp[startX + y][startY + unitLength - x - 1];
 					}
 				}
 			}
 		}
 	}
 
+	// 부분 배열 단위 상하 반전
+	static void op5(int unitLength) {
+		for (int startX = 0; startX < length; startX += unitLength) {
+			for (int startY = 0; startY < length; startY += unitLength) {
 
-
-	static void op5(int subArrayLength) {
-		for (int startX = 0; startX < boardLength; startX += subArrayLength) {
-			for (int startY = 0; startY < boardLength; startY += subArrayLength) {
-
-				int newStartX = boardLength - subArrayLength - startX;
+				int newStartX = length - unitLength - startX;
 				int newStartY = startY;
 
-				// 배열 붙여넣기
-				for (int x = 0; x < subArrayLength; x++) {
-					for (int y = 0; y < subArrayLength; y++) {
-						board[newStartX + x][newStartY + y] = temp[startX + x][startY + y];
+				for (int x = 0; x < unitLength; x++) {
+					for (int y = 0; y < unitLength; y++) {
+						original[newStartX + x][newStartY + y] = temp[startX + x][startY + y];
 					}
 				}
 			}
 		}
 	}
 
-	static void op6(int subArrayLength) {
-		for (int startX = 0; startX < boardLength; startX += subArrayLength) {
-			for (int startY = 0; startY < boardLength; startY += subArrayLength) {
+	// 부분 배열 단위 좌우 반전
+	static void op6(int unitLength) {
+		for (int startX = 0; startX < length; startX += unitLength) {
+			for (int startY = 0; startY < length; startY += unitLength) {
 
 				int newStartX = startX;
-				int newStartY = boardLength - subArrayLength - startY;
+				int newStartY = length - unitLength - startY;
 
-				// 배열 붙여넣기
-				for (int x = 0; x < subArrayLength; x++) {
-					for (int y = 0; y < subArrayLength; y++) {
-						board[newStartX + x][newStartY + y] = temp[startX + x][startY + y];
+				for (int x = 0; x < unitLength; x++) {
+					for (int y = 0; y < unitLength; y++) {
+						original[newStartX + x][newStartY + y] = temp[startX + x][startY + y];
 					}
 				}
 			}
 		}
 	}
 
-	static void op7(int subArrayLength) {
-		for (int startX = 0; startX < boardLength; startX += subArrayLength) {
-			for (int startY = 0; startY < boardLength; startY += subArrayLength) {
+	// 부분 배열 단위 시계 방향 90도 회전
+	static void op7(int unitLength) {
+		for (int startX = 0; startX < length; startX += unitLength) {
+			for (int startY = 0; startY < length; startY += unitLength) {
 
-				int newStartX = (startY / subArrayLength) * subArrayLength;
-				int newStartY = (boardLength / subArrayLength - startX / subArrayLength - 1) * subArrayLength;
+				int newStartX = (startY / unitLength) * unitLength;
+				int newStartY = (length / unitLength - startX / unitLength - 1) * unitLength;
 
-				// 배열 붙여넣기
-				for (int x = 0; x < subArrayLength; x++) {
-					for (int y = 0; y < subArrayLength; y++) {
-						board[newStartX + x][newStartY + y] = temp[startX + x][startY + y];
+				for (int x = 0; x < unitLength; x++) {
+					for (int y = 0; y < unitLength; y++) {
+						original[newStartX + x][newStartY + y] = temp[startX + x][startY + y];
 					}
 				}
 			}
 		}
 	}
 
-	static void op8(int subArrayLength) {
-		for (int startX = 0; startX < boardLength; startX += subArrayLength) {
-			for (int startY = 0; startY < boardLength; startY += subArrayLength) {
+	// 부분 배열 단위 반시계 방향 90도 회전
+	static void op8(int unitLength) {
+		for (int startX = 0; startX < length; startX += unitLength) {
+			for (int startY = 0; startY < length; startY += unitLength) {
 
-				int newStartX = (boardLength / subArrayLength - startY / subArrayLength - 1) * subArrayLength;
-				int newStartY = (startX / subArrayLength) * subArrayLength;
+				int newStartX = (length / unitLength - 1 - startY / unitLength) * unitLength;
+				int newStartY = (startX / unitLength) * unitLength;
 
-				// 배열 붙여넣기
-				for (int x = 0; x < subArrayLength; x++) {
-					for (int y = 0; y < subArrayLength; y++) {
-						board[newStartX + x][newStartY + y] = temp[startX + x][startY + y];
+				for (int x = 0; x < unitLength; x++) {
+					for (int y = 0; y < unitLength; y++) {
+						original[newStartX + x][newStartY + y] = temp[startX + x][startY + y];
 					}
 				}
 			}
 		}
 	}
 
-	static void printBoard() {
-		StringBuilder sb = new StringBuilder();
-
-		for (int row = 0; row < boardLength; row++) {
-			for (int col = 0; col < boardLength; col++) {
-				sb.append(board[row][col]).append(' ');
-			}
-			sb.append('\n');
-		}
-
-		System.out.println(sb);
-	}
-
-	static void copyOriginalBoardToTemp() {
-		for (int row = 0; row < boardLength; row++) {
-			for (int col = 0; col < boardLength; col++) {
-				temp[row][col] = board[row][col];
+	static void copyOriginalToTemp() {
+		for (int x = 0; x < length; x++) {
+			for (int y = 0; y < length; y++) {
+				temp[x][y] = original[x][y];
 			}
 		}
 	}
@@ -220,17 +197,29 @@ public class Main {
 	static void init() {
 		n = scan.nextInt();
 		r = scan.nextInt();
+		length = 1 << n;
 
-		// 한변의 길이
-		boardLength = (int)Math.pow(2, n);
-		board = new int[boardLength][boardLength];
-		temp = new int[boardLength][boardLength];
+		original = new int[length][length];
+		temp = new int[length][length];
 
-		for (int row = 0; row < boardLength; row++) {
-			for (int col = 0; col < boardLength; col++) {
-				board[row][col] = scan.nextInt();
+		for (int row = 0; row < length; row++) {
+			for (int col = 0; col < length; col++) {
+				original[row][col] = scan.nextInt();
 			}
 		}
+	}
+
+	static void printAnswer() {
+		StringBuilder sb = new StringBuilder();
+
+		for (int row = 0; row < length; row++) {
+			for (int col = 0; col < length; col++) {
+				sb.append(original[row][col]).append(' ');
+			}
+			sb.append('\n');
+		}
+
+		System.out.println(sb);
 	}
 
 	static class FastReader {
@@ -252,5 +241,6 @@ public class Main {
 		int nextInt() {
 			return Integer.parseInt(next());
 		}
+
 	}
 }
