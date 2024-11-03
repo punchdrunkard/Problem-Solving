3430291ed6ee96fbfad9d8fecb01064ea01ac4d1
dar-jeeling <o_o___t@pusan.ac.kr>
2024-11-03 21:38:23 +0900

    필요없는 파일 삭제

--- LeetCode/JS_Challenge/Day7/2629.Function Composition.ts
+++
@@ -1,9 +0,0 @@
-// https://leetcode.com/problems/function-composition/?utm_campaign=PostD7&utm_medium=Post&utm_source=Post&gio_link_id=4PY7wZM9
-
-type F = (x: number) => number;
-
-function compose(functions: F[]): F {
-  return function (x): number {
-    return functions.reduceRight((acc, fn) => fn(acc), x);
-  };
-}
--- LeetCode/JS_Challenge/Day9/Approach4.js
+++
@@ -1,4 +0,0 @@
-let memoize4 =
-  (fn, cache = {}) =>
-  (...args) =>
-    cache[args.join()] ?? (cache[args.join()] = fn(...args));
