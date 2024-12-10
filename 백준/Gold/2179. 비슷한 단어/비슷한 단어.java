e233da24e5e60c3a5cb4fd756eb4142ef11a414d
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2024-12-10 18:41:28 +0900

    [Gold III] Title: 비슷한 단어, Time: 156 ms, Memory: 18980 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -5,43 +5,7 @@ public class Main {
 
 	static FastReader scan = new FastReader();
 	static int n;
-
-	static Map<String, Integer> wordsToIdx;
-	static String[] words, original;
-
-	static class Pair implements Comparable<Pair> {
-		int i, j;
-
-		Pair(int i, int j) {
-			int smaller = i;
-			int bigger = j;
-
-			if (i > j) {
-				smaller = j;
-				bigger = i;
-			}
-
-			this.i = smaller;
-			this.j = bigger;
-		}
-
-		@Override
-		public int compareTo(Pair o) {
-			if (this.i == o.i) {
-				return Integer.compare(this.j, o.j);
-			}
-
-			return Integer.compare(this.i, o.i);
-		}
-
-		@Override
-		public String toString() {
-			return "Pair{" +
-				"i=" + i +
-				", j=" + j +
-				'}';
-		}
-	}
+	static String[] words;
 
 	public static void main(String[] args) {
 		init();
@@ -49,60 +13,77 @@ public class Main {
 	}
 
 	static void solve() {
-		int maxCount = -1;
+		// todo 가능한 최대 접두사 길이 찾기
+		int lo = -1;
+		int hi = 101;
 
-		List<Pair> maxList = new LinkedList<>();
+		while (lo + 1 < hi) {
+			int mid = lo + (hi - lo) / 2;
 
-		for (int i = 0; i < words.length; i++) {
-			for (int j = 0; j < i; j++) {
-				if (i == j)
-					continue;
-
-				int currentCount = countPrefix(words[i], words[j]);
-
-				if (maxCount < currentCount) {
-					maxCount = currentCount;
-					maxList = new LinkedList<>();
-					maxList.add(new Pair(wordsToIdx.get(words[i]), wordsToIdx.get(words[j])));
-					continue;
-				}
-
-				if (maxCount == currentCount) {
-					maxList.add(new Pair(wordsToIdx.get(words[i]), wordsToIdx.get(words[j])));
-					continue;
-				}
+			// 현재 단어에서 길이가 mid인 prefix 가 존재하는가?
+			if (hasDuplicatePrefix(mid, words)) {
+				lo = mid;
+			} else {
+				hi = mid;
 			}
 		}
+		
+		// 현재 prefix가 처음으로 등장한 index를 저장한다.
+		Map<String, Integer> prefixToIndex = new HashMap<>();
+		String firstString = null;
+		String secondString = null;
 
-		Collections.sort(maxList);
-		System.out.println(original[maxList.get(0).i] + '\n' + original[maxList.get(0).j]);
+		int earliestIdx = Integer.MAX_VALUE;
+
+		for (int idx = 0; idx < n; idx++) {
+			if (words[idx].length() < lo) {
+				continue;
+			}
+
+			String currentPrefix = words[idx].substring(0, lo);
+
+			if (prefixToIndex.containsKey(currentPrefix)) {
+				int previousIdx = prefixToIndex.get(currentPrefix);
+
+				if (earliestIdx > previousIdx) {
+					earliestIdx = previousIdx;
+					firstString = words[previousIdx];
+					secondString = words[idx];
+				}
+			}
+
+			prefixToIndex.put(currentPrefix, idx);
+		}
+
+		System.out.println(firstString);
+		System.out.println(secondString);
 	}
 
-	static int countPrefix(String s, String t) {
-		int count = 0;
+	static boolean hasDuplicatePrefix(int mid, String[] words) {
+		Set<String> prefixes = new HashSet<>();
 
-		for (int i = 0; i < Math.min(s.length(), t.length()); i++) {
-			if (s.charAt(i) == t.charAt(i)) {
-				count++;
-			} else {
-				break;
+		for (int i = 0; i < n; i++) {
+			if (words[i].length() < mid) {
+				continue;
 			}
+
+			String prefix = words[i].substring(0, mid);
+
+			if (prefixes.contains(prefix)) {
+				return true;
+			}
+
+			prefixes.add(prefix);
 		}
 
-		return count;
+		return false;
 	}
 
 	static void init() {
 		n = scan.nextInt();
 		words = new String[n];
-		original = new String[n];
-
-		wordsToIdx = new HashMap<>();
-
 		for (int i = 0; i < n; i++) {
 			words[i] = scan.next();
-			original[i] = words[i];
-			wordsToIdx.put(words[i], i);
 		}
 	}
 
