95657c651937d39017a8ffed69345380028c69e4
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2024-12-20 16:19:31 +0900

    Time: 35 ms (85.57%), Space: 42.5 MB (76.08%) - LeetHub

--- LeetCode/Hard/0312-burst-balloons/0312-burst-balloons.java
+++ LeetCode/Hard/0312-burst-balloons/0312-burst-balloons.java
@@ -1,48 +1,40 @@
 class Solution {
+    static int[] balloons;
 
-    static int[] nums;
-    static int[][] cache;
-
-    public int maxCoins(int[] input) {
-        init(input);
-        return dp(1, nums.length - 2);
+    public int maxCoins(int[] nums) {
+        init(nums);
+        return solve();
     }
 
-    int dp(int i, int j) {
-        if (i > j) {
-            return 0;
+    public int solve() {
+        int n = balloons.length;
+        int[][] dp = new int[n][n];
+
+        for (int len = 2; len < n; len++) { // 구간 크기: 최소 3부터 시작
+            for (int st = 0; st < n - len; st++) {
+                int en = st + len;
+
+                dp[st][en] = 0; // 초기화
+
+                // 중간 풍선을 마지막으로 터뜨릴 때의 최대 점수 계산
+                for (int mid = st + 1; mid < en; mid++) {
+                    dp[st][en] = Math.max(dp[st][en],
+                            dp[st][mid] + dp[mid][en] + balloons[st] * balloons[mid] * balloons[en]);
+                }
+            }
         }
 
-        if (cache[i][j] != -1) {
-            return cache[i][j];
-        }
-
-        cache[i][j] = 0;
-
-        // i ~ j 구간에서 mid를 마지막으로 터뜨리는 경우 고려
-        for (int mid = i; mid <= j; mid++) {
-            int coins = nums[i - 1] * nums[mid] * nums[j + 1]
-                    + dp(i, mid - 1)
-                    + dp(mid + 1, j);
-
-            cache[i][j] = Math.max(cache[i][j], coins);
-        }
-
-        return cache[i][j];
+        return dp[0][n - 1];
     }
 
-    void init(int[] input) {
-        nums = new int[input.length + 2];
-        nums[0] = 1; // 왼쪽 경계
-        nums[nums.length - 1] = 1; // 오른쪽 경계
-        for (int i = 0; i < input.length; i++) {
-            nums[i + 1] = input[i]; // 입력값 복사
-        }
-
+    void init(int[] nums) {
         int n = nums.length;
-        cache = new int[n][n];
+        balloons = new int[n + 2];
+        balloons[0] = 1;
+        balloons[n + 1] = 1;
+
         for (int i = 0; i < n; i++) {
-            Arrays.fill(cache[i], -1);
+            balloons[i + 1] = nums[i];
         }
     }
 }
