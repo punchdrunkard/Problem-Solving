1df7fe132a35377954da8e558a767c4132813591
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2024-12-20 17:18:13 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,22 +1,32 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int totalSum = Arrays.stream(nums).sum();
-        if (totalSum % 2 == 1) {
+        int sum = Arrays.stream(nums).sum();
+        if (sum % 2 == 1) {
             return false;
         }
 
-        int target = totalSum / 2;
-        boolean[] dp = new boolean[totalSum + 1];
-        dp[0] = true;
+        int target = sum / 2;
 
-        for (int num: nums) {
-            for (int i = target; i >= num; i--) {
-                // 각 num 에 대해, 사용하거나 (dp[target - num], 사용하지 않거나 (dp[i])
-                dp[i] = dp[i] || dp[i - num];
+        // dp[i][j] := i번째 원소까지 고려했을 때, 합이 j를 만족시킬 수 있는가?
+        boolean[][] dp = new boolean[nums.length][sum + 1];
+        // base case
+        for (int i = 0; i < nums.length; i++) {
+            dp[i][0] = true; // 전부 선택안하면 무조건 가능하므로
+        }
+
+        // 첫 번째 원소
+        dp[0][nums[0]] = true;
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (j - nums[i] < 0) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - nums[i]];
+                }
             }
         }
 
-
-        return dp[target];
+        return dp[nums.length - 1][target];
     }
 }
