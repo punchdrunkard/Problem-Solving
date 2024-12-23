36e7d0277cbda22f8ce7619c3e71d2bc7236aada
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2024-12-23 17:33:28 +0900

    Time: 0 ms (100%), Space: 41 MB (50.36%) - LeetHub

--- LeetCode/Medium/0213-house-robber-ii/0213-house-robber-ii.java
+++ LeetCode/Medium/0213-house-robber-ii/0213-house-robber-ii.java
@@ -3,17 +3,25 @@ class Solution {
     static int[] nums, cache;
 
     public int rob(int[] nums) {
-        init(nums);
+        this.nums = nums;
+        return solve();
+    }
 
+    int solve() {
         if (nums.length == 1) {
             return nums[0];
         }
 
+        cache = new int[nums.length];
+
+        Arrays.fill(cache, -1);
         int withoutFirstHouse = dp(1, nums.length - 1);
-        Arrays.fill(cache, -1); // cache 초기화
+
+        Arrays.fill(cache, -1); 
         int withoutLastHouse = dp(0, nums.length - 2);
-  
+
         return Math.max(withoutFirstHouse, withoutLastHouse);
+
     }
     
     int dp(int start, int end) {
@@ -31,11 +39,5 @@ class Solution {
 
         cache[start] = Math.max(selected, notSelected);
         return cache[start];
-    }
-
-    void init(int[] nums) {
-        this.nums = nums;
-        cache = new int[nums.length];
-        Arrays.fill(cache, -1);
     }
 }
