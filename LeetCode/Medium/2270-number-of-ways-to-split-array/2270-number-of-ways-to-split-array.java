b98d7f075067b2a047ec4d5c8370008ffccdbe81
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2025-01-03 17:57:02 +0900

    Time: 2 ms (100%), Space: 58.8 MB (46.66%) - LeetHub

--- LeetCode/Medium/2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ LeetCode/Medium/2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -1,6 +1,10 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        long sum = Arrays.stream(nums).asLongStream().sum();
+        long sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        
         int count = 0;
 
         long left = 0;
