44fdf8fa4aba04c0de7ed5327e3bede066c34d11
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2025-01-03 17:46:36 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Medium/2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    static int n;
+    static int[] pSum, pSumReversed;
+
+    public int waysToSplitArray(int[] nums) {
+        init(nums);
+        calculatePrefixSum(nums);
+
+        int count = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (pSum[i] >= pSumReversed[i + 1]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    static void calculatePrefixSum(int[] nums) {
+        pSum[0] = nums[0];
+        pSumReversed[n - 1] = nums[n - 1];
+
+        for (int i = 1; i < n; i++) {
+            pSum[i] = pSum[i - 1] + nums[i];
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            pSumReversed[i] = pSumReversed[i + 1] + nums[i];
+        }
+    }
+
+    static void init(int[] nums) {
+        n = nums.length;
+        pSum = new int[n];
+        pSumReversed = new int[n];
+    }
+}
