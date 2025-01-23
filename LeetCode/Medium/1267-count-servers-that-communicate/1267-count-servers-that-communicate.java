e470ad4435b1ad38a45aac8291f5475cbe4235ef
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2025-01-23 18:52:29 +0900

    Time: 2 ms (100%), Space: 52.6 MB (38.37%) - LeetHub

--- LeetCode/Medium/1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
+++ LeetCode/Medium/1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
@@ -1,44 +1,32 @@
 class Solution {
-
-    static final int SERVER = 1;
-
-    record Pair(int x, int y) {
-    };
-
     public int countServers(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
 
-        Set<Pair> res = new HashSet<>();
+        int[] rowCount = new int[m];
+        int[] colCount = new int[n];
 
         for (int i = 0; i < m; i++) {
-            Set<Pair> serverInIthRow = new HashSet<>();
-
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == SERVER) {
-                    serverInIthRow.add(new Pair(i, j));
+                if (grid[i][j] == 1) {
+                    rowCount[i]++;
+                    colCount[j]++;
                 }
-            }
-
-            if (serverInIthRow.size() > 1) {
-                res.addAll(serverInIthRow);
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            Set<Pair> serverInIthCol = new HashSet<>();
+        int result = 0;
 
-            for (int j = 0; j < m; j++) {
-                if (grid[j][i] == SERVER) {
-                    serverInIthCol.add(new Pair(j, i));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    if (rowCount[i] > 1 || colCount[j] > 1) {
+                        result++;
+                    }
                 }
-            }
-
-            if (serverInIthCol.size() > 1) {
-                res.addAll(serverInIthCol);
             }
         }
 
-        return res.size();
+        return result;
     }
 }
