31d6e2ce0795629e013ed151fef8df9b897f76a8
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2025-02-20 19:20:24 +0900

    Time: 0 ms (100%), Space: 41.3 MB (87.34%) - LeetHub

--- LeetCode/Medium/1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ LeetCode/Medium/1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,33 +1,12 @@
 class Solution {
-    Set<String> numSet;
-    String answer;
-
     public String findDifferentBinaryString(String[] nums) {
-        numSet = new HashSet<>(Arrays.asList(nums));
         int n = nums[0].length();
-        backtracking(new StringBuilder(), n);
-        return answer;
-    }
-
-    // if it finds answer return true.
-    boolean backtracking(StringBuilder currentStr, int n) {
-        if (currentStr.length() == n) {
-            if (!numSet.contains(currentStr.toString())) {
-                answer = currentStr.toString();
-                return true;
-            } else {
-                return false;
-            }
+        StringBuilder answer = new StringBuilder();
+        
+        for (int i = 0; i < nums.length; i++) {
+            answer.append(nums[i].charAt(i) == '0' ? '1' : '0');
         }
 
-        for (char c : new char[] { '0', '1' }) {
-            currentStr.append(c);
-            if (backtracking(currentStr, n)) {
-                return true;
-            }
-            currentStr.setLength(currentStr.length() - 1);
-        }
-
-        return false;
+        return answer.toString();
     }
 }
