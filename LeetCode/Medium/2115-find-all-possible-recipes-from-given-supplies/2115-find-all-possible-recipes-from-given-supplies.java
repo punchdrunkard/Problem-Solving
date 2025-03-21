b2f5960c7ab3f8bba4f286e726ce4255b9f5ac74
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2025-03-21 15:42:14 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
+++ LeetCode/Medium/2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -1,35 +1,49 @@
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        Set<String> available = new HashSet<>(Arrays.asList(supplies));
-        Queue<Integer> recipeQueue = new LinkedList<>();
-        for (int idx = 0; idx < recipes.length; ++idx) {
-            recipeQueue.offer(idx);
+        Set<String> availableSupplies = new HashSet<>(Arrays.asList(supplies));
+        Map<String, Integer> recipesToIdx = new HashMap<>();
+        List<Integer>[] adj = new ArrayList[recipes.length];
+        for (int recipesIdx = 0; recipesIdx < recipes.length; recipesIdx++) {
+            adj[recipesIdx] = new ArrayList<>();
+            recipesToIdx.put(recipes[recipesIdx], recipesIdx);
+        }
+
+        int[] inDegree = new int[recipes.length];
+        
+        // make dependency graph
+        for (int recipesIdx = 0; recipesIdx < recipes.length; recipesIdx++) {
+            for (String ingredient: ingredients.get(recipesIdx)) {
+                // 재료에 다른 레시피가 포함되어 있음
+                if (!availableSupplies.contains(ingredient)) {
+                    if (recipesToIdx.get(ingredient) != recipesIdx) {
+                        adj[recipesToIdx.get(ingredient)].add(recipesIdx);
+                    }
+                    inDegree[recipesIdx]++;
+                }
+            }
         }
 
         List<String> createdRecipes = new ArrayList<>();
-        int lastSize = -1;
+        
+        // 위상정렬
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < inDegree.length; i++) {
+            if (inDegree[i] == 0) {
+                q.offer(i);
+            }
+        }
 
-        while (available.size() > lastSize) {
-            lastSize = available.size();
-            int queueSize = recipeQueue.size();
+        while (!q.isEmpty()) { // inDegree == 0 => 만들어졌음을 의미 
+            int recipeIndex = q.poll();
+            String created = recipes[recipeIndex];
+            createdRecipes.add(created);
+            
+            // 해당 recipe 가 사용되는 다른 레시피의 inDegree를 줄여주자
+            for (int nextIdx: adj[recipeIndex]) {
+                inDegree[nextIdx]--;
 
-            for (int i = 0; i < queueSize; i++) {
-                int recipeIdx = recipeQueue.poll();
-                boolean canCreate = true;
-
-                for (int j = 0; j < ingredients.get(recipeIdx).size(); j++) {
-                    String need = ingredients.get(recipeIdx).get(j);
-
-                    if (!available.contains(need)) {
-                        canCreate = false;
-                        recipeQueue.offer(recipeIdx);
-                        break;
-                    }
-                }
-
-                if (canCreate) {
-                    createdRecipes.add(recipes[recipeIdx]);
-                    available.add(recipes[recipeIdx]);
+                if (inDegree[nextIdx] == 0) {
+                    q.offer(nextIdx);
                 }
             }
         }
