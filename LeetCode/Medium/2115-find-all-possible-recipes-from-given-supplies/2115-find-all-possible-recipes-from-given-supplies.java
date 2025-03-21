19d0ed9101a415f330e2df58d45e5825ce3cb007
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2025-03-21 15:41:37 +0900

    Time: 568 ms (5.07%), Space: 46.6 MB (79.14%) - LeetHub

--- LeetCode/Medium/2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
+++ LeetCode/Medium/2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -1,36 +1,39 @@
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        Set<String> suppliesSet = new HashSet<>(Arrays.asList(supplies));
-        Set<String> haveIngredients = new HashSet<>(Arrays.asList(supplies));
-        
-        List<String> result = new ArrayList<>();
-        boolean added;
-        
-        do {
-            added = false;
+        Set<String> available = new HashSet<>(Arrays.asList(supplies));
+        Queue<Integer> recipeQueue = new LinkedList<>();
+        for (int idx = 0; idx < recipes.length; ++idx) {
+            recipeQueue.offer(idx);
+        }
 
-            for (int i = 0; i < recipes.length; i++) {
-                if (haveIngredients.contains(recipes[i])) {
-                    continue;
-                }
-                
-                boolean canMake = true;
-                
-                for (String ingredient : ingredients.get(i)) {
-                    if (!haveIngredients.contains(ingredient)) {
-                        canMake = false;
+        List<String> createdRecipes = new ArrayList<>();
+        int lastSize = -1;
+
+        while (available.size() > lastSize) {
+            lastSize = available.size();
+            int queueSize = recipeQueue.size();
+
+            for (int i = 0; i < queueSize; i++) {
+                int recipeIdx = recipeQueue.poll();
+                boolean canCreate = true;
+
+                for (int j = 0; j < ingredients.get(recipeIdx).size(); j++) {
+                    String need = ingredients.get(recipeIdx).get(j);
+
+                    if (!available.contains(need)) {
+                        canCreate = false;
+                        recipeQueue.offer(recipeIdx);
                         break;
                     }
                 }
-            
-                if (canMake) {
-                    haveIngredients.add(recipes[i]);
-                    result.add(recipes[i]);
-                    added = true;
+
+                if (canCreate) {
+                    createdRecipes.add(recipes[recipeIdx]);
+                    available.add(recipes[recipeIdx]);
                 }
             }
-        } while (added);
-        
-        return result;
+        }
+
+        return createdRecipes;
     }
 }
