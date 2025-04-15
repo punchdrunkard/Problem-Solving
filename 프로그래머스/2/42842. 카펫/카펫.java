9c79fb9e45a76c2bcc030f98f567ab7b69c83ad3
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2025-04-15 12:21:23 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,20 @@
 class Solution {
-    // 가능한 모든 r과 c에 대하여 조건을 만족하는지 확인한다.
-    
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        for (int r = 1; r <= 2_000_000; r++) {
-            for (int c = 1; c <= 2_000_000; c++) {
-                int b = 2 * r + 2 * c - 4;
-                int y = r * c - b;
-                if (b == brown && y == yellow && r >= c) {
-                    answer[0] = r;
-                    answer[1] = c;
+        int area = brown + yellow;
+        
+        // 가능한 width, height 에 대하여, 조건을 만족시켜야 한다. 
+        for (int height = 3; height * height <= area; height++) {
+            if (area % height == 0) {
+                int width = area / height;
+                
+                if ((2 * height + 2 * width - 4) == brown) {
+                    answer[0] = width;
+                    answer[1] = height;
                     return answer;
                 }
-            }
+             }
         }
         
         return answer;
