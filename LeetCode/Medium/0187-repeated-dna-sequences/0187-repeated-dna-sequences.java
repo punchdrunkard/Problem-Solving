10cf2180b09d53f7b3e660081f3ea93efc8a920b
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2025-04-24 13:34:05 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ LeetCode/Medium/0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,27 +1,15 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        Map<String, Integer> freqMap = new HashMap<>();
+        Set<String> seen = new HashSet<>();
+        Set<String> repeated = new HashSet<>();
 
-        int right = 0;
-
-        for (int left = 0; left < s.length(); left++) {
-
-            // TODO 10글자가 되면 멈추기 
-            while (right < s.length() && right - left + 1 < 10) {
-                right++;
-            }
-
-            if ((right - left + 1) == 10 && right < s.length()) {
-                String subString = s.substring(left, right + 1);
-                freqMap.put(subString, freqMap.getOrDefault(subString, 0) + 1);
+        for (int i = 0; i < s.length() - 10; i++) {
+            String substring = s.substring(i, i + 10);
+            if (!seen.add(substring)) {
+                repeated.add(substring);
             }
         }
 
-        List<String> result = freqMap.entrySet().stream()
-                .filter(entry -> entry.getValue() > 1)
-                .map(entry -> entry.getKey())
-                .toList();
-
-        return result;
+        return new ArrayList<>(repeated);
     }
 }
