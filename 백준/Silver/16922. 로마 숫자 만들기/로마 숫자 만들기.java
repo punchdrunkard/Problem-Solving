e35b815e23e164c6381c83df4a3a32e8200da48b
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2025-06-16 14:56:02 +0900

    [Silver III] Title: 로마 숫자 만들기, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.java
+++ 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.java
@@ -9,33 +9,40 @@ import java.util.StringTokenizer;
 public class Main {
 
 	static FastReader scan = new FastReader();
-    
+
 	static int[] unit = {1, 5, 10, 50};
+
 	static int n;
 	static Set<Integer> results;
 
 	public static void main(String[] args) {
 		init();
-		solve();
+		dfs(0, 0, 0);
 		System.out.println(results.size());
 	}
 
-	static void solve() {
-		// n을 4개의 집합으로 나눈 후, 각 집합에 로마 숫자를 배정한다. (중복 조합)
-		// i + j + k + l = n
-		for (int i = 0; i <= n; i++) {
-			for (int j = 0; j <= n - i; j++) {
-				for (int k = 0; k <= n - i - j; k++) {
-					int l = n - i - j - k;
+	/**
+	 * 중복 조합 생성
+	 *
+	 * @param k     현재 선택한 로마 숫자의 인덱스 (0~3)
+	 * @param count 현재까지 선택한 문자의 개수
+	 * @param sum   현재까지 계산된 합
+	 */
+	static void dfs(int k, int count, int sum) {
+		// Base Case 1: n개의 문자를 모두 선택했을 때
+		if (count == n) {
+			results.add(sum);
+			return;
+		}
 
-					if (l < 0) {
-						continue;
-					}
-                    
-					int result = (unit[0] * i) + (unit[1] * j) + (unit[2] * k) + (unit[3] * l);
-					results.add(result);
-				}
-			}
+		// Base Case 2: 모든 로마 숫자(I, V, X, L)를 다 고려했을 때
+		if (k == 4) {
+			return;
+		}
+
+		// Recursive Step: 현재 로마 숫자(unit[k])를 i개 선택하는 경우
+		for (int i = 0; i <= n - count; i++) {
+			dfs(k + 1, count + i, sum + (unit[k] * i));
 		}
 	}
 
@@ -51,8 +58,7 @@ public class Main {
 		public FastReader() {
 			br = new BufferedReader(new InputStreamReader(System.in));
 		}
-
-
+        
 		String next() {
 			while (st == null || !st.hasMoreElements()) {
 				try {
