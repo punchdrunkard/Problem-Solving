0f99699c892d3c2a8a5b0105c88a6181f1f9c8e9
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2025-08-05 17:39:53 +0900

    [Silver III] Title: 칸토어 집합, Time: 176 ms, Memory: 21852 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -11,42 +11,31 @@ public class Main {
 	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) {
+		String[] res = preprocess();
+
 		while (scan.hasNext()) {
 			int n = scan.nextInt();
-			solve(n);
+			sb.append(res[n]).append('\n');
 		}
-	}
 
-	static void solve(int n) {
-		//  시작 문자열 -> `-이 n개
-		// 집합 배열의 크기
-		int len = (int)Math.pow(3, n);
-		boolean[] blank = new boolean[len];
-
-		go(0, len - 1, blank);
-
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < blank.length; i++) {
-			sb.append(blank[i] ? ' ' : '-');
-		}
 		System.out.println(sb);
 	}
 
-	static void go(int lo, int hi, boolean[] blank) {
-		// base case
-		if (hi - lo + 1 == 1) {
-			return;
+	// bottom up 방식
+	static String[] preprocess() {
+		String[] arr = new String[13]; // n번째 칸토어 집합
+
+		// arr[0] = -
+		// arr[1] = - - => arr[0] + ' ' + arr[0];
+		// arr[2] = - -   - - => arr[1] + 공백 3칸 (3^1) + arr[1]
+		// arr[3] = arr[2] + 공백 9칸 (3^2) + arr[2]
+		arr[0] = "-";
+		for (int i = 1; i <= 12; i++) {
+			String blank = " ".repeat((int)Math.pow(3, i - 1));
+			arr[i] = arr[i - 1].concat(blank).concat(arr[i - 1]);
 		}
 
-		// recursive case
-		int unit = (hi - lo + 1) / 3;
-		// 3등분 한 후, 가운데 문자열을 공백으로 바꾼다.
-		for (int i = 0; i < unit; i++) {
-			blank[lo + unit + i] = true;
-		}
-
-		go(lo, lo + unit - 1, blank); // for left
-		go(lo + unit + unit, hi, blank); // for right
+		return arr;
 	}
 
 	static class FastReader {
