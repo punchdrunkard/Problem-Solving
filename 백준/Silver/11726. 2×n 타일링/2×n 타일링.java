f8f1fca1d6854522d93b10a786958e0ac5a25824
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2025-10-01 20:28:33 +0900

    [Silver III] Title: 2×n 타일링, Time: 112 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static FastReader scan = new FastReader();
+	static final int MOD = 10_007;
+
+	public static void main(String[] args) {
+		int n = scan.nextInt();
+		System.out.println(solve(n));
+	}
+
+	static int solve(int n) {
+		// init
+		int[] dp = new int[1001];
+		dp[1] = 1;
+		dp[2] = 2;
+
+		for (int i = 3; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+			dp[i] %= MOD;
+		}
+
+		return dp[n];
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String fileName) {
+			try {
+				br = new BufferedReader(new FileReader(fileName));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		boolean hasNext() {
+			while (st == null || !st.hasMoreTokens()) {
+				try {
+					String line = br.readLine();
+					if (line == null) { // EOF에 도달하면 false 반환
+						return false;
+					}
+					st = new StringTokenizer(line);
+				} catch (IOException e) {
+					e.printStackTrace();
+					return false;
+				}
+			}
+			return true;
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+	}
+}
