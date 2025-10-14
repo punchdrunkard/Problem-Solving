8579299fe65a4ae5c5d93ba57bb65a404b13990d
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2025-10-14 20:30:28 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 132 ms, Memory: 14576 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,105 +1,91 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	static FastReader scan = new FastReader();
 	static int n;
-	static int[] a;
+
+	// dpIncrease[i] := a[i]를 마지막으로 하는 가장 긴 *증가하는* 부분 수열의 길이
+	// dpDecrease[i] := a[i]를 시작으로 하는 가장 긴 *감소하는* 부분 수열의 길이
+	static int[] a, dpIncrease, dpDecrease;
 
 	public static void main(String[] args) {
 		init();
-		int answer = solve();
+
+		solveDpIncrease();
+		solveDpDecrease();
+
+		int answer = -1;
+		for (int i = 0; i < n; i++) {
+			answer = Math.max(answer, dpIncrease[i] + dpDecrease[i] - 1);
+		}
+
 		System.out.println(answer);
 	}
 
-	static int solve() {
-		int answer = 1;
-
+	static void solveDpIncrease() {
 		for (int i = 0; i < n; i++) {
-			int k = i;
-
-			// 증가하는 부분
-			int increasingLength = findMaxIncreasingLength(k);
-			// 감소하는 부분
-			int decreasingLength = findMaxDecreasingLength(k);
-			if (decreasingLength == 1 && increasingLength == 1) {
-				continue;
-			}
-
-			// System.out.println("increasingLength = " + increasingLength);
-			// System.out.println("decreasingLength = " + decreasingLength);
-			// System.out.println("k = " + k);
-			answer = Math.max(answer, increasingLength + decreasingLength - 1);
-		}
-
-		return answer;
-	}
-
-	static int findMaxIncreasingLength(int k) {
-		int[] dp = new int[n];
-
-		// dp[i] := i번 index 까지, i번 원소를 포함하는 가장 긴 증가하는 ~
-		dp[0] = 1;
-
-		for (int i = 1; i <= k; i++) {
-			dp[i] = 1;
-
 			for (int j = 0; j < i; j++) {
 				if (a[i] > a[j]) {
-					dp[i] = Math.max(dp[i], dp[j] + 1);
+					dpIncrease[i] = Math.max(dpIncrease[j], dpIncrease[i]);
+
 				}
 			}
+			// 자기 자신 포함
+			dpIncrease[i]++;
 		}
-
-		return dp[k];
 	}
 
-	static int findMaxDecreasingLength(int k) {
-		int[] dp = new int[n];
+	static void solveDpDecrease() {
+		dpIncrease[0] = 1;
 
-		// dp[i] := i를 포함하여, i에서 시작하는 가장 긴 감소 수열의 길이
-		dp[n - 1] = 1;
-
-		for (int i = n - 1; i >= k; i--) {
-			dp[i] = 1;
-
-			for (int j = i + 1; j < n; j++) {
-				if (a[j] < a[i]) {
-					dp[i] = Math.max(dp[i], dp[j] + 1);
+		for (int i = n - 1; i >= 0; i--) {
+			for (int j = i; j < n; j++) {
+				if (a[i] > a[j]) {
+					dpDecrease[i] = Math.max(dpDecrease[j], dpDecrease[i]);
 				}
 			}
+			// 자기 자신 포함
+			dpDecrease[i]++;
 		}
-
-		return dp[k];
 	}
 
 	static void init() {
 		n = scan.nextInt();
 		a = new int[n];
+		dpIncrease = new int[n];
+		dpDecrease = new int[n];
+
 		for (int i = 0; i < n; i++) {
 			a[i] = scan.nextInt();
 		}
 	}
 
 	static class FastReader {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br;
 		StringTokenizer st;
 
-		int nextInt() {
-			return Integer.parseInt(next());
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
 		}
 
 		String next() {
-			try {
-				while (st == null || !st.hasMoreTokens()) {
+			while (st == null || !st.hasMoreElements()) {
+				try {
 					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
-			} catch (IOException e) {
-				e.printStackTrace();
 			}
-
 			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
 		}
 	}
 }
