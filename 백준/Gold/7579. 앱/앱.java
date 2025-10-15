0456eb60546ade658d7400516cd1f81e81370571
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2025-10-15 19:07:49 +0900

    [Gold III] Title: 앱, Time: 128 ms, Memory: 22388 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -1,90 +1,78 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	static FastReader scan = new FastReader();
+
 	static int n, m;
-	static int[] using, deactivateCosts;
-	static int[][] cache;
+	static int[] a, c;
 
 	public static void main(String[] args) {
 		init();
-		int answer = Integer.MAX_VALUE;
-
-		for (int cost = 0; cost <= 10000; cost++) {
-			if (dp(n, cost) >= m) {
-				answer = cost;
-				break;
-			}
-		}
-		System.out.println(answer);
+		System.out.println(solve());
 	}
 
-	// dp[idx][cost]: idx개의 앱을 고려하고, cost 비용으로 확보할 수 있는 최대 메모리
-	static int dp(int idx, int cost) {
-		// Base Case: 앱이 없으면 확보할 메모리가 없음
-		if (idx == 0) {
-			return 0;
+	static int solve() {
+		// preprocess dp
+
+		// dp[i][c] := i번째 인덱스까지 봤을 때, 비용 c로 만들 수 있는 메모리의 최댓값
+		long[][] dp = new long[n + 1][100 * 100 + 1];
+		for (int i = 1; i <= n; i++) {
+			for (int cost = 0; cost <= 100 * 100; cost++) {
+				dp[i][cost] = dp[i - 1][cost];
+				if (cost - c[i] >= 0) {
+					dp[i][cost] = Math.max(dp[i][cost], dp[i - 1][cost - c[i]] + a[i]);
+				}
+			}
 		}
 
-		// 메모이제이션 반환
-		if (cache[idx][cost] != -1) {
-			return cache[idx][cost];
+		for (int cost = 0; cost <= 100 * 100; cost++) {
+			if (dp[n][cost] >= m) {
+				return cost;
+			}
 		}
 
-		// 현재 앱을 종료하지 않는 경우
-		int result = dp(idx - 1, cost);
-
-		// 현재 앱을 종료하는 경우
-		if (cost >= deactivateCosts[idx - 1]) {
-			result = Math.max(result,
-				dp(idx - 1, cost - deactivateCosts[idx - 1]) + using[idx - 1]
-			);
-		}
-
-		// 결과 저장
-		cache[idx][cost] = result;
-		return result;
+		return -1;
 	}
 
 	static void init() {
 		n = scan.nextInt();
 		m = scan.nextInt();
-		using = new int[n];
-		deactivateCosts = new int[n];
-		for (int i = 0; i < n; i++) {
-			using[i] = scan.nextInt();
+		a = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			a[i] = scan.nextInt();
 		}
-
-		for (int i = 0; i < n; i++) {
-			deactivateCosts[i] = scan.nextInt();
-		}
-
-		cache = new int[n + 1][10001];
-		for (int i = 0; i <= n; i++) {
-			Arrays.fill(cache[i], -1);
+		c = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			c[i] = scan.nextInt();
 		}
 	}
 
 	static class FastReader {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br;
 		StringTokenizer st;
 
-		int nextInt() {
-			return Integer.parseInt(next());
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
 		}
 
 		String next() {
-			try {
-				while (st == null || !st.hasMoreTokens()) {
+			while (st == null || !st.hasMoreElements()) {
+				try {
 					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
-			} catch (IOException e) {
-				e.printStackTrace();
 			}
-
 			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
 		}
 	}
 }
