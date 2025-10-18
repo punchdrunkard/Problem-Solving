fb4907aecae18993c2ea7125ffb884df63eef055
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2025-10-18 21:24:27 +0900

    Time: 2 ms (99.01%), Space: 45.1 MB (32.36%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.java
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -1,15 +1,14 @@
-// Hash
 class Solution {
+    // 어떤 원소 k에 대해서 배열의 원소 중 target - k가 존재하는 지 판단해야 한다. 
     public int[] twoSum(int[] nums, int target) {
+        // 배열을 순회하지 않아도 원소가 존재했는지 파악하기 위함
         Map<Integer, Integer> map = new HashMap<>();
-
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-            
             if (map.containsKey(complement)) {
-                return new int[]{map.get(complement), i};
+                return new int[]{i, map.get(complement)};
             }
-
+            
             map.put(nums[i], i);
         }
 
