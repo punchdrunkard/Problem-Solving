bef2ae29c9c2322e69efc86abcf25365177792b5
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2025-10-24 22:49:28 +0900

    Time: 1 ms (99.41%), Space: 57 MB (71.51%) - LeetHub

--- LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.java
+++ LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,17 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int size = nums.length;
-        int[] dp = new int[size];
-
-        int result = nums[0];
+        int n = nums.length;
+        // dp[i] := nums[i]를 반드시 포함하는 Maximum Subarray
+        int[] dp = new int[n];
+        int max = nums[0];
         dp[0] = nums[0];
-
-        for (int i = 1; i < size; i++) {
-            // add element to previous seq or start from i-th index 
-            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
-            result = Math.max(result, dp[i]);
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
+            max = Math.max(dp[i], max);
         }
 
-        return result;
+        return max;
     }
 }
