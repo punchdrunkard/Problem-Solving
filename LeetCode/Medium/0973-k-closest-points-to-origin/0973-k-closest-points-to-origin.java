2a4bb1eacc05281aa5c1f788c2bc08c263a644e1
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2025-10-28 18:51:10 +0900

    Time: 28 ms (67.24%), Space: 56.2 MB (5.96%) - LeetHub

---
+++ LeetCode/Medium/0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        Arrays.sort(points, (a, b) -> {
+            long aDist = (long) a[0] * a[0] + (long) a[1] * a[1];
+            long bDist = (long) b[0] * b[0] + (long) b[1] * b[1];
+            return Long.compare(aDist, bDist);
+        });
+        
+        return Arrays.stream(points).limit(k).toArray(int[][]::new);
+    }
+}
